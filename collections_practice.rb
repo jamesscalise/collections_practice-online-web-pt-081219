e7c41177be68e22fc4251e434b5ad1ddc7506376
James Scalise <jamescscalise@gmail.com>
2019-08-19 23:05:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+def sort_array_char_count(array)
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+end
+
+def kesha_maker(array)
+end
+
+def find_a(array)
+end
+
+def sum_array(array)
+end
+
+def add_s(array)
+end
